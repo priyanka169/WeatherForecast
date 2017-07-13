a91749a8fe2d010f124eaa2c678b4d41f512643c
priyanka169 <priyanka.pinki222@gmail.com>
2017-07-13 23:05:25 +0530

    Weather Forecast

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+
+import App from './components/app';
+import rootReducer from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(rootReducer)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
